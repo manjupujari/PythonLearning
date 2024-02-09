db5173d53f982a838ca422438346a5cd141914fd
= <manjunathpujari819@gmail.com>
2024-02-09 16:17:31 +0530

    Variables Committing

---
+++ variables.py
@@ -0,0 +1,46 @@
+# variable_name = variable_value
+
+print(100)
+print("Manju")
+username = "Manjunath"
+print(username)
+print('username')
+print("username")
+print(type(username))
+
+z = 10.14
+print(type(z))
+
+x = 600
+print(x)
+print(type(x))
+
+x = 800
+print(x)
+
+# Ex-1
+# id defines the address of the variable_name
+
+x = 100
+print(x)
+y = x
+print(y)
+print(id(x))
+print(id(y))
+
+# Ex-2
+
+a = 200
+print(a)
+b = 300
+print(b)
+print(id(a))
+print(id(b))
+
+Str = "Hello Manju"
+print(Str)
+
+# we can assign values to variables in a single line also
+b, c, d, e = 5, 6.4, "Great", True
+
+print(b, c, d, e)
